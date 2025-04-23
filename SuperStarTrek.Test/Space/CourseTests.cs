4f027b1bf8a1c7da17a0f36afbe39af963be6782
kamiesull <kamiesull10@gmail.com>
2025-04-23 15:10:12 -0700

    Set up Test project

---
+++ SuperStarTrek.Test/Space/CourseTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperStartTrek.Space;
+
+public class CourseTests
+{
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
