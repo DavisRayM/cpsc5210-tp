2afbfc08acd503b192654c4485b7604a8f4ed205
macnoblebk <macnoblebk@gmail.com>
2025-05-07 19:23:41 -0700

    Creates PhotonTubesTest file and setups test fixture

---
+++ SuperStarTrek.Test/Systems/PhotonTubesTest.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using Games.Common.IO;
+using Games.Common.Randomness;
+using SuperStarTrek.Systems;
+using SuperStarTrek.Objects;
+using SuperStarTrek.Space;
+using SuperStarTrek.Commands;
+using NUnit.Framework;
+
+namespace SuperStarTrek.Test.Systems
+{
+	[TestFixture]
+	public class PhotonTubesTest
+	{
+		
+		private Mock<IReadWrite> _mockIO;
+		private Enterprise _enterprise;
+		private Mock<IQuadrant> _mockQuadrant;
+		private PhotonTubes _photonTubes;
+		private const int INITIAL_TORPEDO_COUNT = 10;
+
+		
+
+	
+
+
+
+
+
+
+
+    }
+}
+
