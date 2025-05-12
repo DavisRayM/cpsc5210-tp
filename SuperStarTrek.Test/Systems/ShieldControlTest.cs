44be09f516068f916563a40acd59dbbf6049e8dc
macnoblebk <macnoblebk@gmail.com>
2025-05-12 15:56:51 -0700

    Adds additional test

--- SuperStarTrek.Test/Systems/ShieldControlTest.cs
+++ SuperStarTrek.Test/Systems/ShieldControlTest.cs
@@ -12,6 +12,20 @@ namespace SuperStarTrek.Test.Systems
 	public class ShieldControlTest
 	{
         [Test]
+        public void AbsorbHit_ReducesShieldEnergy()
+        {
+            var mockIO = new Mock<IReadWrite>();
+            var mockRandom = new Mock<IRandom>();
+            var mockEnterprise = new Mock<Enterprise>(10, new Coordinates(1, 1), mockIO.Object, mockRandom.Object);
+
+            var shieldControl = new ShieldControl(mockEnterprise.Object, mockIO.Object);
+            shieldControl.ShieldEnergy = 500;
+            shieldControl.AbsorbHit(200);
+            Assert.AreEqual(300, shieldControl.ShieldEnergy);
+        }
+
+
+        [Test]
         public void CanExecuteCommand_WhenOperational_ReturnsTrue()
         {
             var mockIO = new Mock<IReadWrite>();
@@ -34,6 +48,24 @@ namespace SuperStarTrek.Test.Systems
             Assert.IsFalse(shieldControl.CanExecuteCommand());
             mockIO.Verify(io => io.WriteLine("Shield Control inoperable"), Times.Once);
         }
+
+        //[Test]
+        //public void ExecuteCommandCore_DisplaysAvailableEnergy()
+        //{
+        //    var mockIO = new Mock<IReadWrite>();
+        //    var mockRandom = new Mock<IRandom>();
+        //    var mockEnterprise = new Mock<Enterprise>(10, new Coordinates(1, 1), mockIO.Object, mockRandom.Object);
+        //    var mockQuadrant = new Mock<IQuadrant>();
+        //    var shieldControl = new ShieldControl(mockEnterprise.Object, mockIO.Object);
+
+        //    mockEnterprise.Setup(e => e.TotalEnergy).Returns(1000);
+        //    mockIO.Setup(io => io.ReadNumber("Number of units to shields")).Returns(500);
+
+        //    shieldControl.ExecuteCommandCore(mockQuadrant.Object);
+
+        //    mockIO.Verify(io => io.WriteLine("Energy available = 1000"), Times.Once);
+        //}
+
 
     }
 }
