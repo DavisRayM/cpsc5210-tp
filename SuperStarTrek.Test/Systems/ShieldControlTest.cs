3f819b622e0668bae1b165fdadf245f8a55226f3
macnoblebk <macnoblebk@gmail.com>
2025-06-02 12:00:34 -0700

    Splits multi-assert tests into focused single-purpose tests

--- SuperStarTrek.Test/Systems/ShieldControlTest.cs
+++ SuperStarTrek.Test/Systems/ShieldControlTest.cs
@@ -1,6 +1,7 @@
 ﻿using Games.Common.IO;
 using Games.Common.Randomness;
 using Moq;
+using NUnit.Framework;
 using SuperStarTrek.Commands;
 using SuperStarTrek.Objects;
 using SuperStarTrek.Space;
@@ -58,6 +59,19 @@ namespace SuperStarTrek.Test.Systems
             var shieldControl = new ShieldControl(mockEnterprise.Object, mockIO.Object);
             shieldControl.TakeDamage(1);
             Assert.IsFalse(shieldControl.CanExecuteCommand());
+        }
+
+        [Test]
+        public void CanExecuteCommand_WhenDamaged_DisplaysInoperableMessage()
+        {
+            var mockIO = new Mock<IReadWrite>();
+            var mockRandom = new Mock<IRandom>();
+            var mockEnterprise = new Mock<Enterprise>(10, new Coordinates(1, 1), mockIO.Object, mockRandom.Object);
+            var shieldControl = new ShieldControl(mockEnterprise.Object, mockIO.Object);
+            shieldControl.TakeDamage(1);
+
+            shieldControl.CanExecuteCommand();
+
             mockIO.Verify(io => io.WriteLine("Shield Control inoperable"), Times.Once);
         }
 
@@ -79,61 +93,108 @@ namespace SuperStarTrek.Test.Systems
         }
 
         [Test]
-        public void ExecuteCommandCore_WithNegativeRequest_Rejects()
+        public void ExecuteCommandCore_WithNegativeRequest_LeavesShieldEnergyUnchanged()
         {
             var mockIO = new Mock<IReadWrite>();
             var mockRandom = new Mock<IRandom>();
             var mockEnterprise = new Mock<Enterprise>(1000, new Coordinates(1, 1), mockIO.Object, mockRandom.Object);
             var mockQuadrant = new Mock<IQuadrant>();
             var shieldControl = new ShieldControl(mockEnterprise.Object, mockIO.Object);
+            mockIO.Setup(io => io.ReadNumber("Number of units to shields")).Returns(-100);
 
-           
+            shieldControl.ExecuteCommandCore(mockQuadrant.Object);
+
+            Assert.AreEqual(0, shieldControl.ShieldEnergy);
+        }
+
+        [Test]
+        public void ExecuteCommandCore_WithNegativeRequest_DisplaysUnchangedMessage()
+        {
+            var mockIO = new Mock<IReadWrite>();
+            var mockRandom = new Mock<IRandom>();
+            var mockEnterprise = new Mock<Enterprise>(1000, new Coordinates(1, 1), mockIO.Object, mockRandom.Object);
+            var mockQuadrant = new Mock<IQuadrant>();
+            var shieldControl = new ShieldControl(mockEnterprise.Object, mockIO.Object);
+            mockIO.Setup(io => io.ReadNumber("Number of units to shields")).Returns(-100);
+
+            shieldControl.ExecuteCommandCore(mockQuadrant.Object);
+
+            mockIO.Verify(io => io.WriteLine("<SHIELDS UNCHANGED>"), Times.Once);
+        }
+
+        [Test]
+        public void ExecuteCommandCore_WithNegativeRequest_ReturnsOkResult()
+        {
+            var mockIO = new Mock<IReadWrite>();
+            var mockRandom = new Mock<IRandom>();
+            var mockEnterprise = new Mock<Enterprise>(1000, new Coordinates(1, 1), mockIO.Object, mockRandom.Object);
+            var mockQuadrant = new Mock<IQuadrant>();
+            var shieldControl = new ShieldControl(mockEnterprise.Object, mockIO.Object);
             mockIO.Setup(io => io.ReadNumber("Number of units to shields")).Returns(-100);
 
             var result = shieldControl.ExecuteCommandCore(mockQuadrant.Object);
 
-            Assert.AreEqual(0, shieldControl.ShieldEnergy);
-            mockIO.Verify(io => io.WriteLine("<SHIELDS UNCHANGED>"), Times.Once);
             Assert.AreEqual(CommandResult.Ok, result);
         }
 
         [Test]
-        public void ExecuteCommandCore_WithSameAsCurrent_Rejects()
+        public void ExecuteCommandCore_WithSameAsCurrent_LeavesShieldEnergyUnchanged()
         {
             var mockIO = new Mock<IReadWrite>();
             var mockRandom = new Mock<IRandom>();
             var mockEnterprise = new Mock<Enterprise>(1000, new Coordinates(1, 1), mockIO.Object, mockRandom.Object);
             var mockQuadrant = new Mock<IQuadrant>();
             var shieldControl = new ShieldControl(mockEnterprise.Object, mockIO.Object);
-
             shieldControl.ShieldEnergy = 300;
-           
             mockIO.Setup(io => io.ReadNumber("Number of units to shields")).Returns(300);
 
-            var result = shieldControl.ExecuteCommandCore(mockQuadrant.Object);
+            shieldControl.ExecuteCommandCore(mockQuadrant.Object);
 
             Assert.AreEqual(300, shieldControl.ShieldEnergy);
-            mockIO.Verify(io => io.WriteLine("<SHIELDS UNCHANGED>"), Times.Once);
-            Assert.AreEqual(CommandResult.Ok, result);
         }
 
         [Test]
-        public void ExecuteCommandCore_WithExcessiveRequest_RejectsWithMessage()
+        public void ExecuteCommandCore_WithExcessiveRequest_DisplaysTreasuryMessage()
         {
             var mockIO = new Mock<IReadWrite>();
             var mockRandom = new Mock<IRandom>();
             var mockEnterprise = new Mock<Enterprise>(1000, new Coordinates(1, 1), mockIO.Object, mockRandom.Object);
             var mockQuadrant = new Mock<IQuadrant>();
             var shieldControl = new ShieldControl(mockEnterprise.Object, mockIO.Object);
+            mockIO.Setup(io => io.ReadNumber("Number of units to shields")).Returns(1500);
 
-         
+            shieldControl.ExecuteCommandCore(mockQuadrant.Object);
+
+            mockIO.Verify(io => io.WriteLine("Shield Control reports, 'This is not the Federation Treasury.'"), Times.Once);
+        }
+
+        [Test]
+        public void ExecuteCommandCore_WithExcessiveRequest_DisplaysUnchangedMessage()
+        {
+            var mockIO = new Mock<IReadWrite>();
+            var mockRandom = new Mock<IRandom>();
+            var mockEnterprise = new Mock<Enterprise>(1000, new Coordinates(1, 1), mockIO.Object, mockRandom.Object);
+            var mockQuadrant = new Mock<IQuadrant>();
+            var shieldControl = new ShieldControl(mockEnterprise.Object, mockIO.Object);
+            mockIO.Setup(io => io.ReadNumber("Number of units to shields")).Returns(1500);
+
+            shieldControl.ExecuteCommandCore(mockQuadrant.Object);
+
+            mockIO.Verify(io => io.WriteLine("<SHIELDS UNCHANGED>"), Times.Once);
+        }
+
+        [Test]
+        public void ExecuteCommandCore_WithExcessiveRequest_ReturnsOkResult()
+        {
+            var mockIO = new Mock<IReadWrite>();
+            var mockRandom = new Mock<IRandom>();
+            var mockEnterprise = new Mock<Enterprise>(1000, new Coordinates(1, 1), mockIO.Object, mockRandom.Object);
+            var mockQuadrant = new Mock<IQuadrant>();
+            var shieldControl = new ShieldControl(mockEnterprise.Object, mockIO.Object);
             mockIO.Setup(io => io.ReadNumber("Number of units to shields")).Returns(1500);
 
             var result = shieldControl.ExecuteCommandCore(mockQuadrant.Object);
 
-            Assert.AreEqual(0, shieldControl.ShieldEnergy);
-            mockIO.Verify(io => io.WriteLine("Shield Control reports, 'This is not the Federation Treasury.'"), Times.Once);
-            mockIO.Verify(io => io.WriteLine("<SHIELDS UNCHANGED>"), Times.Once);
             Assert.AreEqual(CommandResult.Ok, result);
         }
 
