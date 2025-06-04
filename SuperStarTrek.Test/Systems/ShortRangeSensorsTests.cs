33367c417cef58087b657770f83be29fed318313
jladhar <jladhar@seattleu.edu>
2025-06-04 14:50:20 -0700

    updating shortrangesensors tests

--- SuperStarTrek.Test/Systems/ShortRangeSensorsTests.cs
+++ SuperStarTrek.Test/Systems/ShortRangeSensorsTests.cs
@@ -57,22 +57,27 @@ namespace SuperStarTrek.Test.Systems
                     var mockRandom = new Mock<IRandom>();
                     var mockEnterprise = new Mock<Enterprise>(1, new Coordinates(0, 0), mockIO.Object, mockRandom.Object);
                     var mockGalaxy = new Mock<Galaxy>(mockRandom.Object);
+                    mockGalaxy.SetupGet(g => g.KlingonCount).Returns(10);
                     var mockGame = new Mock<Game>(mockIO.Object, mockRandom.Object);
                     var quadrant = new Mock<IQuadrant>();
                     var pt = new Mock<PhotonTubes>(15, mockEnterprise.Object, mockIO.Object);
                     mockEnterprise.SetupGet(e => e.Condition).Returns("Good");
                     quadrant.Setup(q => q.GetDisplayLines()).Returns(new List<string>
                     {
-                        "SEC1", "SEC2", "SEC3", "SEC4", "SEC5", "SEC6"
+                        "SEC1", "SEC2", "SEC3", "SEC4", "SEC5", "SEC6", "SEC7", "SEC8"
                     });
                     mockGame.SetupGet(game => game.Stardate).Returns(1);
                     var c1 = new Coordinates(1, 1);
                     mockEnterprise.SetupGet(e => e.QuadrantCoordinates).Returns(c1);
                     mockEnterprise.SetupGet(e => e.PhotonTubes).Returns(pt.Object);
+                    var mockShield = new Mock<ShieldControl>(mockEnterprise.Object, mockIO.Object);
+                    mockEnterprise.Object.Add(mockShield.Object);
+                    mockShield.SetupGet(s => s.ShieldEnergy).Returns(50);
                     pt.SetupGet(p => p.TorpedoCount).Returns(5);
                     var srs = new ShortRangeSensors(mockEnterprise.Object, mockGalaxy.Object, mockGame.Object,_ioSpy);
 
                     var result = srs.ExecuteCommandCore(quadrant.Object);
+
 
                     Assert.That(_ioSpy.GetOutput(), Is.EqualTo(string.Join(Environment.NewLine,
                                 [
@@ -83,6 +88,8 @@ namespace SuperStarTrek.Test.Systems
                                     " SEC4         Sector             1 , 1",
                                     " SEC5         Photon torpedoes   5",
                                     " SEC6         Total energy       1",
+                                    " SEC7         Shields            50",
+                                    " SEC8         Klingons remaining 10",
                                     "---------------------------------",
                                     ""
                                 ])));
