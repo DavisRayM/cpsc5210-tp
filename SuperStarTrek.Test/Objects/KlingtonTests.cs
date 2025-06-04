81abe00c8deb73928489e01071b0ae7e3a26be84
jladhar <jladhar@seattleu.edu>
2025-06-04 14:04:01 -0700

    updating klingon tests

---
+++ SuperStarTrek.Test/Objects/KlingtonTests.cs
@@ -0,0 +1,73 @@
+using Moq;
+using NUnit.Framework;
+using SuperStarTrek.Objects;
+using Games.Common.Randomness;
+using SuperStarTrek.Space;
+using SuperStarTrek.Commands;
+
+namespace SuperStarTrek.Test.Systems
+{
+    public class KlingonTests
+    {
+        private Mock<IRandom> _mockRandom;
+        private Klingon _klingon;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockRandom = new Mock<IRandom>();
+
+
+            _mockRandom.Setup(r => r.NextFloat()).Returns(0.5f);
+
+
+            _klingon = new Klingon(new Coordinates(0, 0), _mockRandom.Object);
+        }
+
+        [Test]
+        public void Constructor_SetsInitialEnergyBasedOnRandom()
+        {
+
+            Assert.That(_klingon.Energy, Is.EqualTo(200).Within(0.01));
+        }
+
+        [Test]
+        public void MoveTo_UpdatesSector()
+        {
+            var newSector = new Coordinates(5, 7);
+            _klingon.MoveTo(newSector);
+            Assert.That(_klingon.Sector, Is.EqualTo(newSector));
+        }
+
+        [Test]
+        public void TakeHit_HitGreaterThanThreshold_ReturnsTrueAndReducesEnergy()
+        {
+            float initialEnergy = _klingon.Energy;
+
+
+            int hitStrength = (int)(0.2 * initialEnergy);
+
+            bool result = _klingon.TakeHit(hitStrength);
+
+            Assert.IsTrue(result);
+            Assert.That(_klingon.Energy, Is.LessThan(initialEnergy));
+        }
+
+        [Test]
+        public void TakeHit_HitLessThanThreshold_ReturnsFalseAndDoesNotReduceEnergy()
+        {
+            float initialEnergy = _klingon.Energy;
+
+
+            int hitStrength = (int)(0.1 * initialEnergy);
+
+            bool result = _klingon.TakeHit(hitStrength);
+
+            Assert.IsFalse(result);
+            Assert.That(_klingon.Energy, Is.EqualTo(initialEnergy));
+        }
+
+
+
+    }
+}
